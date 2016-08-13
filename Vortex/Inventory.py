b9b685e81ac02f7bc1cabaef00d148f5ccbf955d
kyzaghan <kyzaghan@ismailkose.com.tr>
2016-08-13 20:06:42 +0300

    Added inventory information

--- Vortex/Inventory.py
+++ Vortex/Inventory.py
@@ -1,7 +1,67 @@
+# -*- coding: utf-8 -*-
+
+from Util.SettingsReader import  read_config
+from Util.Logger import logger
+
+class Inventory:
+    def __init__(self):
+        self.c = read_config()
+        self.Pokeball = 0
+        self.GreatBall = 0
+        self.UltraBall = 0
+        self.MasterBall = 0
+        self.l = logger()
+
+    def getCurrentPokeBallCount(self):
+        if self.c["PokeBall"] == "Poke Ball":
+            return self.Pokeball
+        elif (self.c["PokeBall"] == "Great Ball"):
+            return self.GreatBall
+        elif (self.c["PokeBall"] == "Ultra Ball"):
+            return self.UltraBall
+        else:
+            return self.MasterBall
+
+    def removeCurrentPokeBallCount(self):
+        if self.c["PokeBall"] == "Poke Ball":
+            self.Pokeball -= 1
+        elif (self.c["PokeBall"] == "Great Ball"):
+            self.GreatBall -= 1
+        elif (self.c["PokeBall"] == "Ultra Ball"):
+            self.UltraBall -= 1
+        else:
+            self.MasterBall -= 1
+
+    def getPokeBallBuyCount(self, type):
+        try :
+            if(type == "Poke Ball" and self.c["PokeBallBuyList"]["" + type +""] > 0) :
+             PokeBallDiff = self.c["PokeBallBuyList"]["" + type +""] - self.Pokeball
+            elif(type == "Great Ball" and self.c["PokeBallBuyList"]["" + type +""] > 0):
+             PokeBallDiff = self.c["PokeBallBuyList"]["" + type +""] - self.GreatBall
+            elif (type == "Ultra Ball" and self.c["PokeBallBuyList"]["" + type +""] > 0):
+             PokeBallDiff = self.c["PokeBallBuyList"]["" + type +""] - self.UltraBall
+            elif (type == "Master Ball" and self.c["PokeBallBuyList"]["" + type +""] > 0):
+             PokeBallDiff = self.c["PokeBallBuyList"]["" + type +""] - self.MasterBall
+            else:
+             PokeBallDiff = 0
+
+            if(PokeBallDiff <= 0) :
+                return 0
+            elif PokeBallDiff >= 100 or PokeBallDiff > 50:
+                return 100
+            elif PokeBallDiff > 25 and PokeBallDiff <= 50 :
+                return 50
+            elif PokeBallDiff > 10 and PokeBallDiff <= 25:
+                return 25
+            elif PokeBallDiff > 5 and PokeBallDiff <= 10:
+                return 10
+            else:
+                return 5
+        except Exception as e:
+            self.l.writelog(str(e), "critical")
 
 class Trainer:
-    class Inventory:
-        Pokeball = 0
-        GreatBall = 0
-        UltraBall = 0
-        MasterBall = 0
+    def __init__(self):
+        self.inventory = Inventory()
+
+
