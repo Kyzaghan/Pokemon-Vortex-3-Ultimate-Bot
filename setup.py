dc583cacdf4f754406cd0fdeb8ba87e2391f7cde
kyzaghan <kyzaghan@ismailkose.com.tr>
2016-08-13 20:54:02 +0300

    Added auto installer and run.bat

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='PokemonVortexV3Catcher',
+    version='0.1.0',
+    author='Kyzaghan',
+    author_email='ik@ismailkose.com.tr',
+    scripts=['run.py'],
+    description='Pokemon Vortex V3 AUTO CATCHER',
+    install_requires=[
+    "beautifulsoup4 >= 4",
+    "requests >= 2",
+],
+)
