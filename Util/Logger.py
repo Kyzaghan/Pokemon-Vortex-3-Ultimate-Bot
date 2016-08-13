2f94bcb15c0318d2f9b5f734f1968c8dd9870bd7
kyzaghan <kyzaghan@ismailkose.com.tr>
2016-08-13 20:05:55 +0300

    Changed log class

--- Util/Logger.py
+++ Util/Logger.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import time
-from Utils.termcolor import cprint
-
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+from Util.termcolor import cprint
 
 class logger:
     def __init__(self):
