2d2d91702460e66b369a626201c11afd4ae17216
kyzaghan <kyzaghan@ismailkose.com.tr>
2016-08-13 20:05:36 +0300

    Added Auto buy pokéballs and show inventory items

--- HttpWrapper/Wrapper.py
+++ HttpWrapper/Wrapper.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
-import requests
-from SettingParser.SettingsReader import read_authentication, read_config, read_map
-from Logging.Logger import logger
 import time
+
+import requests
+
+from Util.Logger import logger
+from Util.SettingsReader import read_authentication, read_config, read_map
+from Vortex.Inventory import Trainer
+from Util.termcolor import cprint
+import re
+
 try:
     from BeautifulSoup import BeautifulSoup
 except ImportError:
@@ -17,6 +23,7 @@ class http_wrapper():
         self.c = read_config()
         self.m = read_map()
         self.l = logger()
+        self.trainer = Trainer()
 
     @property
     def do_login(self):
@@ -36,6 +43,7 @@ class http_wrapper():
 
     def start_bot(self):
         try:
+            self.get_inventory()
             self.find_pokemon()
         except Exception as e:
          self.l.writelog(str(e), "critical")
@@ -43,6 +51,12 @@ class http_wrapper():
     def find_pokemon(self):
         try:
             while(True):
+
+                if self.trainer.inventory.getCurrentPokeBallCount() <= self.c["PokeBallBuyList"][self.c["PokeBall"]]:
+                    cprint("Pokeball is not enough, is purchasing...")
+                    self.purchase_pokeball()
+                    break
+
                 self.l.writelog("Pokemon searching...", "info")
                 tmp_current_map = self.m["MapList"]['' + str(self.c["CurrentMap"]) + '']
                 url = "http://" + self.a["Server"] + ".pokemon-vortex.com/xml/toolbox.php?map=" + str(
@@ -66,8 +80,8 @@ class http_wrapper():
                     self.catch_pokemon(form_id)
                     break
         except Exception as e:
-         self.do_login()
          self.l.writelog(str(e), "critical")
+         self.do_login()
 
     def catch_pokemon(self, FormId):
         try:
@@ -109,10 +123,11 @@ class http_wrapper():
             while(True) :
                 self.l.writelog("Catch started", "info")
                 url = "http://" + self.a["Server"] + ".pokemon-vortex.com/wildbattle.php?&ajax=1"
-                data = {"o1" : o1, "o2" : o2, "o3" : o3, "o4" : o4, "actionattack" : "1", "actionattack" : "1", "bat" : "1", "item" : self.c["PokeBall"],
+                data = {"o1" : o1, "o2" : o2, "o3" : o3, "o4" : o4, "actionattack" : "1", "actionattack" : "1", "bat" : "1", "item" : self.c["PokeBall"].replace("Poke Ball", "Pokeball"),
                         "action" : "use_item", "active_pokemon" : "1" }
                 r = self.s.post(url, data)
-
+                self.trainer.inventory.removeCurrentPokeBallCount()
+                self.print_current_inventory()
                 if("has been caught" in r.text) :
                     url = "http://" + self.a["Server"] + ".pokemon-vortex.com/wildbattle.php?&ajax=1"
                     data = {"action": "1", "bat": "1"}
@@ -125,8 +140,143 @@ class http_wrapper():
                     url = "http://" + self.a["Server"] + ".pokemon-vortex.com/wildbattle.php?&ajax=1"
                     data = {"action": "1", "bat": "1"}
                     r = self.s.post(url, data)
+
+                if self.trainer.inventory.getCurrentPokeBallCount() <= self.c["PokeBallBuyList"][self.c["PokeBall"]]:
+                    cprint("Pokeball is not enough, is purchasing...")
+                    self.purchase_pokeball()
+                    break
+
         except Exception as e:
          self.l.writelog(str(e), "critical")
          self.find_pokemon()
 
+    def get_inventory(self):
+        try:
+            self.l.writelog("Getting Inventory Information!", "info")
+            url = "http://" + self.a["Server"] + ".pokemon-vortex.com/inventory.php"
+            r = self.s.get(url)
+            ph = BeautifulSoup(r.text, "html.parser")
+            tmp_pokeball_div = ph.find_all('div', attrs={"class":"list autowidth"})
+            ph = BeautifulSoup(str(tmp_pokeball_div[1]), "html.parser")
+            ph = BeautifulSoup(str(ph.find_all("tr")), "html.parser")
+            i = 0
+            for tdList in ph.find_all("td") :
+                if(i == 3) : #Pokeball
+                    self.trainer.inventory.Pokeball = int(tdList.text)
+                elif(i == 10) : #Great Ball
+                    self.trainer.inventory.GreatBall = int(tdList.text)
+                elif (i == 17):  # Ultra Ball
+                    self.trainer.inventory.UltraBall = int(tdList.text)
+                elif (i == 24):  # Master Ball
+                    self.trainer.inventory.MasterBall = int(tdList.text)
+                i+=1
+            self.print_current_inventory()
+        except Exception as e:
+         self.l.writelog(str(e), "critical")
 
+    def print_current_inventory(self):
+        cprint("Pokéball = " + str(self.trainer.inventory.Pokeball) + "\n"
+               "Great Ball = " + str(self.trainer.inventory.GreatBall) + "\n"
+               "Ultra Ball = " + str(self.trainer.inventory.UltraBall) + "\n"
+               "Master Ball = " + str(self.trainer.inventory.MasterBall) + "\n",
+               "blue"
+               )
+
+    def purchase_pokeball(self):
+        try :
+            self.l.writelog("Buying pokeballs", "info")
+            url = "http://" + self.a["Server"] + ".pokemon-vortex.com/items.php"
+            data = {"potion":0,
+                    "superpotion":0,
+                    "hyperpotion":0,
+                    "pokeball":self.trainer.inventory.getPokeBallBuyCount("Poke Ball"),
+                    "greatball":self.trainer.inventory.getPokeBallBuyCount("Great Ball"),
+                    "ultraball":self.trainer.inventory.getPokeBallBuyCount("Ultra Ball"),
+                    "masterball":self.trainer.inventory.getPokeBallBuyCount("Master Ball"),
+                    "fullheal":0,
+                    "antidote":0,
+                    "parlyzheal":0,
+                    "burnheal":0,
+                    "iceheal":0,
+                    "awakening":0,
+                    "dawnstone":0,
+                    "duskstone":0,
+                    "firestone":0,
+                    "leafstone":0,
+                    "moonstone":0,
+                    "ovalstone":0,
+                    "shinystone":0,
+                    "sunstone":0,
+                    "thunderstone":0,
+                    "waterstone":0,
+                    "deepseascale":0,
+                    "deepseatooth":0,
+                    "dragonscale":0,
+                    "dubiousdisc":0,
+                    "electirizer":0,
+                    "magmarizer":0,
+                    "kingsrock":0,
+                    "metalcoat":0,
+                    "prismscale":0,
+                    "protector":0,
+                    "razorclaw":0,
+                    "razorfang":0,
+                    "reapercloth":0,
+                    "upgrade":0,
+                    "sachet":0,
+                    "whippeddream":0,
+                    "icerock":0,
+                    "mossrock":0,
+                    "abomasite":0,
+                    "absolite":0,
+                    "aerodactylite":0,
+                    "aggronite":0,
+                    "alakazite":0,
+                    "altarianite":0,
+                    "ampharosite":0,
+                    "audinite":0,
+                    "banettite":0,
+                    "beedrillite":0,
+                    "blastoisinite":0,
+                    "blazikenite":0,
+                    "cameruptite":0,
+                    "charizarditex":0,
+                    "charizarditey":0,
+                    "diancite":0,
+                    "galladite":0,
+                    "garchompite":0,
+                    "gardevoirite":0,
+                    "gengarite":0,
+                    "glalitite":0,
+                    "gyaradosite":0,
+                    "heracronite":0,
+                    "houndoominite":0,
+                    "kangaskhanite":0,
+                    "lopunnite":0,
+                    "lucarionite":0,
+                    "manectite":0,
+                    "mawilite":0,
+                    "medichamite":0,
+                    "metagrossite":0,
+                    "mewtwonitex":0,
+                    "mewtwonitey":0,
+                    "pidgeotite":0,
+                    "pinsirite":0,
+                    "sablenite":0,
+                    "salamencite":0,
+                    "sceptilite":0,
+                    "scizorite":0,
+                    "sharpedonite":0,
+                    "slowbronite":0,
+                    "steelixite":0,
+                    "swampertite":0,
+                    "tyranitarite":0,
+                    "venusaurite":0,
+                    "buy":"Buy Items"
+                    }
+            r = self.s.post(url, data)
+            self.l.writelog("Pokéballs buyed.", "info")
+            self.get_inventory()
+            self.find_pokemon()
+        except Exception as e:
+            self.l.writelog(str(e), "critical")
