0ed7da9239324591f26cb10924b6d984e6bfa618
kyzaghan <kyzaghan@ismailkose.com.tr>
2016-08-18 08:43:15 +0300

    Added version control

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 from Bot.CatcherBot import http_wrapper
 from Bot.ExpBot import pvexpbot
+from Util.VersionManager import version_manager
+import sys
+sys.setrecursionlimit(1000000000)
+vm = version_manager()
+vm.checkVersion()
+print("\n")
 BotType = input("ExpBot or Catcher (E/C)")
+print("\n")
 if(BotType == "E") :
     hw = pvexpbot()
     hw.do_login()
